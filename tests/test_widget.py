9108961b08c8c3e6b9b22088872551add14490df
Charlie Denton <charlie@meshy.co.uk>
2016-06-13 00:03:46 +0100

    Docstring tweak

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -6,7 +6,7 @@ from sirtrevor.widgets import SirTrevorWidget
 class TestSirTrevorWidgetRender(TestCase):
     """Render tests for SirTrevorWidget."""
     def test_render_empty(self):
-        """Just a basic textarea when empty"""
+        """Just a basic textarea when empty."""
         result = SirTrevorWidget().render(name='test', value=None)
         expected = '<textarea name="test">\r\n</textarea>'
         self.assertEqual(result, expected)
