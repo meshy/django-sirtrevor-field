8bd80391680e67410096e7a53bca7bd4302f24f1
Charlie Denton <charlie@meshy.co.uk>
2016-06-12 14:58:44 +0100

    Add django appropriate test runner

---
+++ tests/run.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python
+"""From http://stackoverflow.com/a/12260597/400691."""
+import sys
+
+import dj_database_url
+import django
+from colour_runner.django_runner import ColourRunnerMixin
+from django.conf import settings
+from django.test.runner import DiscoverRunner
+
+
+settings.configure(
+    DATABASES={'default': dj_database_url.config(
+        default='postgres:///sirtrevor',
+    )},
+    INSTALLED_APPS=(
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+    ),
+    MIDDLEWARE_CLASSES=(),
+    PASSWORD_HASHERS=('django.contrib.auth.hashers.MD5PasswordHasher',),
+    ROOT_URLCONF='tests.urls',
+    TEMPLATES=[{
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'APP_DIRS': True,
+    }],
+)
+
+
+django.setup()
+
+
+class TestRunner(ColourRunnerMixin, DiscoverRunner):
+    """Enable coloured output for tests."""
+
+
+test_runner = TestRunner(verbosity=1)
+failures = test_runner.run_tests(None)
+if failures:
+    sys.exit(1)
