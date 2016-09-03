e0702f138017d91c60785c155fdc28d28f8629bd
Charlie Denton <charlie@meshy.co.uk>
2016-09-03 11:49:38 +0100

    Add "login" helper method to ScreenshotTestCase

--- tests/testcases.py
+++ tests/testcases.py
@@ -17,3 +17,17 @@ class ScreenshotTestCase(NeedleTestCase, StaticLiveServerTestCase):
     @classmethod
     def get_web_driver(cls):
         return PhantomJSDriver()
+
+    def login(self, user):
+        """
+        I loathe that we have to log in manually. With a form. Like animals.
+
+        FIXME: Find a way to make this work without filling in a form. Just set
+               a "session" cookie, or something? I tried. I was defeated.
+        """
+        self.driver.get(self.live_server_url + '/admin/')
+        username = self.driver.find_element_by_id('id_username')
+        username.send_keys(user.username)
+        password = self.driver.find_element_by_id('id_password')
+        password.send_keys(user.raw_password)
+        self.driver.find_element_by_css_selector('form').submit()
